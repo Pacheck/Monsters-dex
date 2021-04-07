9950187cb9a4119e4434970532b2ac07f3880c72
Pedro Pacheco <pacheckdev@gmail.com>
2021-04-07 17:51:22 -0300

    change de app from root to container and added styles

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import CardList from './components/card-list';
-import SearchBox from './components/search-box';
+import CardList from '../components/card-list';
+import SearchBox from '../components/search-box';
+
+import './App.css';
 class App extends React.Component {
   constructor(){
     super();
@@ -22,12 +24,18 @@ class App extends React.Component {
       ],
       searchField: '',
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
     fetch(`https://jsonplaceholder.typicode.com/users`)
     .then(res => res.json())
     .then(users => this.setState({ monsters: users}))
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
   }
 
   render(){
@@ -38,9 +46,10 @@ class App extends React.Component {
 
     return(
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder="search monsters" 
-          handleChange={e => this.setState({ searchField: e.target.value })} 
+          handleChange={this.handleChange} 
         />
         <CardList monsters={filteredMonsters}/>
       </div>
